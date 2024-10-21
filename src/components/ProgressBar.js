21f1f43191f8dd3dd8f897f5629b43af1308d52f
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-10-21 17:00:45 +0000

    code

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -2,58 +2,61 @@ import React, { useState, useEffect } from "react";
 
 
 const ProgressBar = () => {
-  const [progress, setProgress] = useState(0);
+    const [progress, setProgress] = useState(0);
 
-  useEffect(() => {
-    const updateProgress = () => {
-      if (progress < 100) {
-        setTimeout(() => {
-          setProgress((prevProgress) => (prevProgress + 10)); // Increase progress by 10%
-        }, 500);
-      }
-    };
+    useEffect(() => {
+        const updateProgress = () => {
+            if (progress < 100) {
+                setTimeout(() => {
+                    setProgress((prevProgress) => (prevProgress + 10)); // Increase progress by 10%
+                }, 500);
+            }
+        };
 
-    updateProgress();
-  }, [progress]); // The effect depends on the 'progress' value
+        updateProgress();
+    }, [progress]); // The effect depends on the 'progress' value
 
-  return (
-    <div>
-      <div id="barOuter" 
-      style={styles.barOuter}
-      >
-        <div
-          id="barInner"
-          style={{
-            ...styles.barInner,
-            width: `${progress}%`,
-          }}
-        >
-          {progress}%
-        </div>
-      </div>
-      <span>{progress}%</span>
-      {progress === 100 && <div>Operation Complete!</div>}
-    </div>
-  );
+    return (
+        <>
+            <h2>Progress Bar</h2>
+            <div>
+                <div id="barOuter"
+                    style={styles.barOuter}
+                >
+                    <div
+                        id="barInner"
+                        style={{
+                            ...styles.barInner,
+                            width: `${progress}%`,
+                        }}
+                    >
+                        {progress}%
+                    </div>
+                </div>
+                <p>{progress}%</p>
+                {progress === 100 && <p>Operation Complete!</p>}
+            </div>
+        </>
+    );
 };
 
 const styles = {
-  barOuter: {
-    width: "25%",
-    height: "30px",
-    backgroundColor: "#f0f0f0",
-    borderRadius: "5px",
-    overflow: "hidden",
-    margin: "20px 0",
-  },
-  barInner: {
-    height: "100%",
-    backgroundColor: "#4caf50",
-    textAlign: "center",
-    lineHeight: "30px",
-    color: "white",
-    transition: "width 1s linear", // Smooth transition
-  },
+    barOuter: {
+        width: "25%",
+        height: "30px",
+        backgroundColor: "#f0f0f0",
+        borderRadius: "5px",
+        overflow: "hidden",
+        margin: "20px 0",
+    },
+    barInner: {
+        height: "100%",
+        backgroundColor: "#4caf50",
+        textAlign: "center",
+        lineHeight: "30px",
+        color: "white",
+        transition: "width 1s linear", // Smooth transition
+    },
 };
 
 
