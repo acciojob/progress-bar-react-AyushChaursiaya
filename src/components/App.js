d22e3a6bfa22dc65a21811e326ebdd6b93c252ac
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-10-21 16:56:30 +0000

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import ProgressBar from "./ProgressBar";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        {/* Do not remove the main div */} 
+        <ProgressBar />
     </div>
   )
 }
